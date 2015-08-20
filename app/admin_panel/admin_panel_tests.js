6ada4c0c7e9d8c73d6f14c70ca9956d6fe765526
Jordan Thompson <jordanthomp81@gmail.com>
2015-08-20 14:26:54 -0500

    changed the admin_panel_test file

--- app/admin_panel/admin_panel_tests.js
+++ app/admin_panel/admin_panel_tests.js
@@ -1,34 +1,72 @@
 'use strict';
 
-var expect = require('chai').expect;
-var request = require('supertest');
+var expect = require('chai').expect
+  , request = require('supertest');
 
-var app = require('../../app/');
+var app = require('../../app')
+  , database = require('../../lib/mongo');
 
-describe('Home Routes', function () {
-  describe('GET /', function(){
-    it('respond with index', function(done){
+describe('Autocomplete', function() {
+  describe('GET /mention', function() {
+
+    before(function(done) { // add users to user collection in db
+      var users = [
+        { name : 'world' },
+        { name : 'work' },
+        { name : 'woman' }
+      ];
+      database.connect(function(err,db) {
+        db.collection('user').drop( // but clear collection first
+          function() {
+            db.collection('user').insert( users ,
+              function(err) {
+                if(err) throw err;
+                done();
+              });
+          });
+      });
+    });
+
+    it('should send an empty array when there\'s no query', function(done) {
       request(app)
-        .get('/')
+        .get('/autocomplete/mention')
         .expect(200)
-        .end(function (err, res) {
-          if (err) throw err;
-          expect(res.text).to.contain("I'm an index page");
+        .end(function(err,res) {
+          if(err) return done(err);
+          expect(res.body).to.eql([]);
           done();
         });
     });
-  });
 
-  describe('GET /contact', function(){
-    it('respond with contact', function(done){
+    it('should respond with json', function(done) {
       request(app)
-        .get('/contact')
+        .get('/autocomplete/mention?pattern=wor')
+        .set('Accept','application/json')
+        .expect('Content-Type',/json/)
+        .expect(200,done);
+    });
+
+    it('should respond with matches and not with non-matches', function(done) {
+      request(app)   // seeking matches for wor
+        .get('/autocomplete/mention?pattern=wor')
         .expect(200)
-        .end(function (err, res) {
-          if (err) throw err;
-          expect(res.text).to.contain("I'm a contact page");
+        .end(function(err, res) {
+          if(err) return done(err);
+          expect(res.body).to.deep.equal([
+            {name: 'world'}, {name: 'work'}
+          ]);
           done();
-        });
+      });
+    });
+
+    after(function(done) {
+      database.connect(function(err,db) {
+        db.collection('user').drop(
+          function(err) {
+            if(err) throw err;
+            done();
+          });
+      });
     });
   });
 });
