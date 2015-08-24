8af373fb8686958f3e28c7dbce966003215f54b6
Jordan Thompson <jordanthomp81@gmail.com>
2015-08-24 13:49:02 -0500

    more changes to the tests

--- app/admin_panel/admin_panel_routes.js
+++ app/admin_panel/admin_panel_routes.js
@@ -4,6 +4,10 @@ var router = express();
 var app = require('../../app')
 var database = require('../../lib/mongo');
 
-app.get('/user', function(req, res){
+app.get('/admin', function(req, res){
+  res.send(200);
+});
+
+app.get('admin/users', function(req, res){
   res.send(200);
 });
