bb4b595da57c59872d41b39a6babcb8f39e6593d
Jordan Thompson <jordanthomp81@gmail.com>
2015-08-24 13:36:38 -0500

    more changes to the testing

--- app/admin_panel/admin_panel_tests.js
+++ app/admin_panel/admin_panel_tests.js
@@ -30,30 +30,38 @@ describe('Admin Dashboard', function() {
 
     it('Should show the dashboard for the admin panel', function(done) {
       request(app)
-        .get('/admin')
-        .expect(200,done);
-    });
-
-    it('Should show a searchable list of users', function(done) {
-      request(app)
-        .get('/admin/users')
+        .get('/admin/')
         .set('Accept','application/json')
         .expect('Content-Type',/json/)
         .expect(200,done);
     });
 
-    it('User search should respond with matches and not with non-matches', function(done) {
-      request(app)   // seeking matches for wor
-        .get('/admin/users?name=Tim')
-        .expect(200)
-        .end(function(err, res) {
-          if(err) return done(err);
-          expect(res.body).to.deep.equal([
-            {name: 'Tim'}, {name: 'Bob'}
-          ]);
-          done();
-      });
-    });
+    // it('Should show the dashboard for the admin panel', function(done) {
+    //   request(app)
+    //     .get('/admin')
+    //     .expect(200,done);
+    // });
+
+    // it('Should show a searchable list of users', function(done) {
+    //   request(app)
+    //     .get('/admin/users')
+    //     .set('Accept','application/json')
+    //     .expect('Content-Type',/json/)
+    //     .expect(200,done);
+    // });
+
+    // it('User search should respond with matches and not with non-matches', function(done) {
+    //   request(app)   // seeking matches for wor
+    //     .get('/admin/users?name=Tim')
+    //     .expect(200)
+    //     .end(function(err, res) {
+    //       if(err) return done(err);
+    //       expect(res.body).to.deep.equal([
+    //         {name: 'Tim'}, {name: 'Bob'}
+    //       ]);
+    //       done();
+    //   });
+    // });
 
     after(function(done) {
       database.connect(function(err,db) {
